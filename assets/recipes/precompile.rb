4df45c75f04a107076619a7f54f67536e974942d
Matt Lins <mattlins@gmail.com>
2014-07-23 20:26:54 -0500

    testing assets

--- assets/recipes/precompile.rb
+++ assets/recipes/precompile.rb
@@ -1,31 +1,27 @@
 node[:deploy].each do |application, deploy|
-  before_symlink do
-    directory "#{deploy[:deploy_to]}/shared/cache" do
-      owner 'deploy'
-      group 'www-data'
-      mode '0770'
-      action :create
-    end
-
-    directory "#{deploy[:deploy_to]}/shared/assets" do
-      owner 'deploy'
-      group 'www-data'
-      mode '0770'
-      action :create
-    end
+  directory "#{deploy[:deploy_to]}/shared/cache" do
+    owner 'deploy'
+    group 'www-data'
+    mode '0770'
+    action :create
   end
 
-  before_restart do
-    execute "precompile assets for Rails app #{application}" do
-      cwd deploy[:current_path]
-      command 'bundle exec rake assets:precompile'
-      environment 'RAILS_ENV' => deploy[:rails_env]
-    end
+  directory "#{deploy[:deploy_to]}/shared/assets" do
+    owner 'deploy'
+    group 'www-data'
+    mode '0770'
+    action :create
+  end
 
-    execute "cleanup assets for Rails app #{application}" do
-      cwd deploy[:current_path]
-      command 'bundle exec rake assets:cleanup[5]'
-      environment 'RAILS_ENV' => deploy[:rails_env]
-    end
+  execute "precompile assets for Rails app #{application}" do
+    cwd deploy[:current_path]
+    command 'bundle exec rake assets:precompile'
+    environment 'RAILS_ENV' => deploy[:rails_env]
+  end
+
+  execute "cleanup assets for Rails app #{application}" do
+    cwd deploy[:current_path]
+    command 'bundle exec rake assets:cleanup[5]'
+    environment 'RAILS_ENV' => deploy[:rails_env]
   end
 end
