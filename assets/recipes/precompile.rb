85b4030328dd9e8b6653ea0794fc2dc1e840b580
Matt Lins <mattlins@gmail.com>
2014-07-26 15:58:34 -0500

    Basic rails environment

--- assets/recipes/precompile.rb
+++ assets/recipes/precompile.rb
@@ -1,13 +1,27 @@
 node[:deploy].each do |application, deploy|
+  directory "#{deploy[:deploy_to]}/shared/cache" do
+    owner 'deploy'
+    group 'www-data'
+    mode '0770'
+    action :create
+  end
+
+  directory "#{deploy[:deploy_to]}/shared/assets" do
+    owner 'deploy'
+    group 'www-data'
+    mode '0770'
+    action :create
+  end
+
   execute "precompile assets for Rails app #{application}" do
     cwd deploy[:current_path]
     command 'bundle exec rake assets:precompile'
     environment 'RAILS_ENV' => deploy[:rails_env]
   end
 
-  execute "cleanup assets for Rails app #{application}" do
+  execute "clean assets for Rails app #{application}" do
     cwd deploy[:current_path]
-    command 'bundle exec rake assets:cleanup[5]'
+    command 'bundle exec rake assets:clean[5]'
     environment 'RAILS_ENV' => deploy[:rails_env]
   end
 end
