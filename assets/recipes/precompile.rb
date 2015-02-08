0f2385b4464c5d7b266df3bc177d6d8b480d861b
Matt Lins <mattlins@gmail.com>
2015-02-08 03:08:58 -0600

    Give devise a key

--- assets/recipes/precompile.rb
+++ assets/recipes/precompile.rb
@@ -2,6 +2,9 @@ node[:deploy].each do |application, deploy|
   execute "precompile assets for Rails app #{application}" do
     cwd deploy[:current_path]
     command 'bundle exec rake assets:precompile'
-    environment 'RAILS_ENV' => deploy[:rails_env]
+    environment ({
+      "RAILS_ENV" => deploy[:rails_env],
+      "SECRET_KEY_BASE" => "devise-needs-this"
+    })
   end
 end
