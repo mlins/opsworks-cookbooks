537c043e8c765f6cae4f5c7dda4a031c5bbd93a6
Matt Lins <mattlins@gmail.com>
2015-02-08 01:38:28 -0600

    Remove env recipes

--- env/attributes/deploy.rb
+++
@@ -1,3 +0,0 @@
-node[:deploy].each do |application, deploy|
-  default[:deploy][application][:symlink_before_migrate]['config/application.yml'] = 'config/application.yml'
-end
--- env/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'env'
-maintainer       'Matt Lins'
-maintainer_email 'mattlins@gmail.com'
-license          'MIT'
-description      'Environment Variables for Opsworks'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
