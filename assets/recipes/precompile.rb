72822678cb24a78f5b649091a724eff66fc63bcd
Matt Lins <mattlins@gmail.com>
2015-02-08 09:44:07 -0600

    Add shared assets

--- assets/recipes/precompile.rb
+++ assets/recipes/precompile.rb
@@ -1,4 +1,12 @@
 node[:deploy].each do |application, deploy|
+  directory "#{deploy[:deploy_to]}/shared/assets" do
+    group params[:group]
+    owner params[:user]
+    mode 0770
+    action :create
+    recursive true
+  end
+
   execute "precompile assets for Rails app #{application}" do
     cwd deploy[:current_path]
     command "bundle exec rake assets:precompile"
